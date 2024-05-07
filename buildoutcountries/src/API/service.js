35929750cf825deff735320fa3ea6fcd268beb42
dream023se <dubeysanchika82@gmail.com>
2024-05-07 16:42:32 +0530

    error

--- buildoutcountries/src/API/service.js
+++ buildoutcountries/src/API/service.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+
+
 export const getCountryFlag=async()=>{
+   
     try{
     const response=await axios.get(`https://restcountries.com/v3.1/all`)
 
@@ -8,7 +11,8 @@ export const getCountryFlag=async()=>{
     }
     catch(e)
     {
-        console.log(e);
+        console.error('Error fetching data:', e.message);
+        
     }
 }
 //https://xbuild-out-countries.vercel.app/
